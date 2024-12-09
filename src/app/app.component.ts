e33502ebd7823ffd0a97ae04199a06e15be96d1e
AxelOjedaHernandez <axelojedahernandez64@gmail.com>
2024-12-09 08:21:54 -0600

    segundo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DirectivasComponent } from './directivas/directivas.component';
+import { EncabezadoComponent } from "./encabezado/encabezado.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    DirectivasComponent
-  ],
+    DirectivasComponent,
+    EncabezadoComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
